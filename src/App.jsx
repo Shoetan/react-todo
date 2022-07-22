6e07352a45f35e1f5581be9e78e1a1ce023ba8a9
unknown <emmysoetan@gmail.com>
2022-07-22 19:56:53 +0100

    made use of a tenary operator to only push task to the array once the input field is not empty

--- src/App.jsx
+++ src/App.jsx
@@ -21,19 +21,29 @@ const handleChange = (event) => {
 /* create a function to add the task to the array todos */
 
 const addTask = () =>{
-  const newTodos = [...todos, newTask]
-  setTodos(newTodos)
+  
+  /* use the tenary operator to add a task only if the input field is not empty */
+
+  newTask !=='' ? setTodos( [...todos, newTask]) : console.log('Enter a task');
+
+  console.log(todos);
   /* Clear the input field once the add task button is clicked */
   setNewTask("")
+ 
 }
 
 
 
   return ( 
     <div className="font-poppins">
+    {/* Input field and button */}
       <div className="container mx-auto justify-center flex gap-4 mt-6">
         <input type="text" onChange={handleChange} value={newTask} placeholder="Create new todo.." className="h-8 w-96 rounded-md p-6 focus:outline-none bg-slate-50"/>
         <button className="bg-blue-400 text-black p-2 rounded-full hover:bg-blue-500" onClick={addTask}>Add Todo</button>
+      </div>
+    {/* Display the list in the array */}
+      <div>
+
       </div>
 
     </div>
